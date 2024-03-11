15c20e421e867a0e00b7498ceff825fb6f919d24
HuiDo Choo <manner0814@sjhansol.sjeduhs.kr>
2024-03-11 09:19:50 +0900

    commit

--- src/jsx/form/ResultForm.jsx
+++ src/jsx/form/ResultForm.jsx
@@ -1,11 +1,14 @@
+import React, { useEffect, useState } from "react";
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import { FORM_ } from "../../js/texts";
 import NextButton from "../widget/NextButton";
-import React, {useEffect, useState} from "react";
 import { PAGE_ } from "../../js/RouteLink";
 import { User } from "../../js/User";
 import { db } from "../../js/firebase";
 import { ref, set } from "firebase/database";
 import styles from '../../css/form/ResultForm.module.css';
+import {valueInvalidWarning} from "../widget/WarningToast";
 
 const ResultForm = () => {
     const [title, setTitle] = useState("잠시만 기다려주세요..");
@@ -18,25 +21,45 @@ const ResultForm = () => {
         if (isUserEmpty()) {
             setTitle("비정상적인 방법으로\n페이지에 접근하셨습니다.");
             setContent("처음부터 다시 시도해주세요.");
+            valueInvalidWarning();
             setShowButton(true);
-        } else {
-            set(ref(db, 'users/' + User.학번), User)
-                .then(() => {
-                    setTitle(FORM_.RESULT_TITLE);
-                    setContent(FORM_.RESULT_CONTENT);
-                    setShowButton(true);
-                })
-                .catch((error) => {
-                    setTitle("데이터 전송에 실패했습니다. 오류: " + error.message);
-                    setContent("처음부터 다시 시도해주십시오.");
-                    setShowButton(true);
-                });
+            return;
         }
-        console.log(User);
+
+        const promise = set(ref(db, 'users/' + User.학번), User);
+
+        toast.promise(
+            promise,
+            {
+                pending: '데이터를 서버에 전송중입니다. \n잠시만 기다려주세요.',
+                success: {
+                    render() {
+                        setTitle(FORM_.RESULT_TITLE);
+                        setContent(FORM_.RESULT_CONTENT);
+                        setShowButton(true);
+                        return '데이터 전송이 완료되었습니다. \n신청해주셔서 감사합니다.';
+                    },
+                },
+                error: {
+                    render({data}) {
+                        setTitle("데이터 전송에 실패했습니다. 오류: " + data.message);
+                        setContent("처음부터 다시 시도해주십시오.");
+                        setShowButton(true);
+                        return '데이터 전송에 실패하였습니다. \n처음부터 다시 시도해주세요';
+                    }
+                }
+            },
+            {
+                position: "top-center",
+                theme: "dark",
+            }
+        ).then(r => {});
+
     }, []);
 
     return(
         <>
+            <ToastContainer />
             <div className={styles['centered-square']}>
                 <p className={styles.title}>{formatStringToJsx(title)}</p>
                 <p className={styles.context}>{formatStringToJsx(content)}</p>
@@ -47,6 +70,7 @@ const ResultForm = () => {
         </>
     );
 }
+
 
 const formatStringToJsx = (str) => {
     return str.split('\n').map((line, index) =>
--- src/jsx/widget/WarningToast.jsx
+++ src/jsx/widget/WarningToast.jsx
@@ -8,7 +8,7 @@ const WarningToast = () => {
         <>
             <ToastContainer
                 position="top-center"
-                autoClose={1700}
+                autoClose={2000}
                 hideProgressBar={false}
                 closeOnClick
                 rtl={false}
@@ -21,8 +21,30 @@ const WarningToast = () => {
     );
 }
 
-const inputEmptyWarning = () => toast.error("모든 필수 필드를 채워주세요.");
-const studentNumberInvalidWarning = () => toast.error("학번은 숫자 5글자로 입력해주세요");
-const phoneNumberInvalidWarning = () => toast.error("올바른 형식의 전화번호를 입력해주세요");
+const inputEmptyWarning = () => {
+    toast.error("모든 필수 필드를 채워주세요.");
+}
 
-export { WarningToast, inputEmptyWarning, studentNumberInvalidWarning, phoneNumberInvalidWarning };
+const studentNumberInvalidWarning = () => {
+    toast.error("학번은 숫자 5글자로 입력해주세요");
+}
+
+const phoneNumberInvalidWarning = () => {
+    toast.error("올바른 형식의 전화번호를 입력해주세요");
+}
+
+const valueInvalidWarning = () => {
+    toast.error("비정상적인 방법으로 페이지에 접근하셨습니다. \n처음부터 다시 시도해주세요.", {
+        position: "top-center",
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        rtl: false,
+        pauseOnFocusLoss: true,
+        draggable: true,
+        pauseOnHover: true,
+        theme: "dark",
+    });
+}
+
+export { WarningToast, inputEmptyWarning, studentNumberInvalidWarning, phoneNumberInvalidWarning, valueInvalidWarning };
